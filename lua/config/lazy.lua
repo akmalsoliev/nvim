0bc0d69dcd35530999da372354ca36f9edde01e5
Akmal Soliev <soliev@gmail.com>
2026-07-18 23:15:08 +0200

    fix: deprecated function

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- this is a directory under nvim/lua/plugins
-    { import = "../plugins/" },
+    { import = "plugins" },
   },
   defaults = {
     lazy = false,
